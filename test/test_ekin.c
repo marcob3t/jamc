dafac0a431056be76fa61a647f3c33b66e8b96ef
Jiaxin Wang <jiaxin@dhcp-128-011.sissa.it>
2018-01-26 11:33:37 +0100

    new ekin test

--- test/test_ekin.c
+++ test/test_ekin.c
@@ -1,47 +1,24 @@
 // reference output of this test should be test_ekin.dat
 #include "ljmd.h"
+#include <stdlib.h>
 
 /* test main */
 int main(int argc, char **argv){
     int i;
-    char restfile[BLEN], line[BLEN];
-    FILE *fp,*fn;
+    FILE *fn;
     mdsys_t sys;
     
-    /* read input file */
-    // number of atoms
-    if(get_a_line(stdin,line)) return 1;
-    sys.natoms=atoi(line);
-    // mass of atoms
-    if(get_a_line(stdin,line)) return 1;
-    sys.mass=atof(line);
-    // keep these trash just to read restfile name
-    if(get_a_line(stdin,line)) return 1;
-    if(get_a_line(stdin,line)) return 1;
-    if(get_a_line(stdin,line)) return 1;
-    if(get_a_line(stdin,line)) return 1;
-    // initial setting file name
-    if(get_a_line(stdin,restfile)) return 1;
-    // alloc velocity field
+    sys.natoms=10;
+    //sys.redmass = 39.948*2390.05736153349; // reduced mass
+    sys.mass = 39.948;
     sys.vx=(double *)malloc(sys.natoms*sizeof(double));
     sys.vy=(double *)malloc(sys.natoms*sizeof(double));
     sys.vz=(double *)malloc(sys.natoms*sizeof(double));
-    // catching un-wanted information
-    double trash[3];
     
-    /* read restart */
-    fp=fopen(restfile,"r");
-    if(fp) {
-        for (i=0; i<sys.natoms; ++i) {
-            fscanf(fp,"%lf%lf%lf",trash, trash+1, trash+2);
-        }
-        for (i=0; i<sys.natoms; ++i) {
-            fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
-        }
-        fclose(fp);
-    } else {
-        perror("cannot read restart file");
-        return 3;
+    for (i=0; i<sys.natoms; ++i) {
+        *(sys.vx+i) = 1e-3*(rand()%10);
+        *(sys.vy+i) = 1e-3*(rand()%10);
+        *(sys.vz+i) = 1e-3*(rand()%10);
     }
     
     ekin(&sys);
