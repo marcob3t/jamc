d0802645d9b37b7e572802b2a2adab0f0c62cbd8
marcob3t <marco.b3t@gmail.com>
2018-01-27 17:17:28 +0100

    naive omp pragmas added

--- src/force.c
+++ src/force.c
@@ -16,57 +16,63 @@ void force(mdsys_t *sys)
     double rcutsq = sys->rcut*sys->rcut;// pre-calculate, take square
     double c6 = sys->epsilon*pow(sys->sigma,6);
     double c12 = sys->epsilon*pow(sys->sigma,12);
-    
-    for(i=0; i < (sys->natoms); ++i) {
-        for(j=0; j < i; ++j) {
+
+#ifdef _OPENMP
+#pragma omp parallel private(i,j, rx, ry, rz, rsq, rsq_inv, r6, ffac) reduction(+,epot)
+#endif
+    {
+#ifdef _OPENMP
+#pragma omp for
+#endif
+        for(i=0; i < (sys->natoms); ++i) {
+            for(j=0; j < i; ++j) {
+                /* particles have no interactions with themselves */
+            
+                /* get distance between particle i and j */
+                rx=pbc(sys->rx[i] - sys->rx[j], boxby2);
+                rsq = rx*rx;
+                if(rsq>rcutsq) continue; // 1D pre truncate
+                ry=pbc(sys->ry[i] - sys->ry[j], boxby2);
+                rsq += ry*ry;
+                if(rsq>rcutsq) continue; // 1D pre truncate
+                rz=pbc(sys->rz[i] - sys->rz[j], boxby2);
+                rsq += rz*rz;
+                if(rsq>rcutsq) continue; // 1D pre truncate
+            
+                rsq_inv = 1.0/rsq;
+                r6 = rsq_inv*rsq_inv*rsq_inv;
+                ffac = (48*c12*r6-24*c6)*r6*rsq_inv;
+                sys->epot += 2*r6*(c12*r6-c6);
+                sys->fx[i] += rx*ffac;
+                sys->fy[i] += ry*ffac;
+                sys->fz[i] += rz*ffac;
+            
+            }
             /* particles have no interactions with themselves */
+            // rm (i==j) decision with separated loop
+            for(j=i+1; j < sys->natoms; ++j) {
+                /* particles have no interactions with themselves */
             
-            /* get distance between particle i and j */
-            rx=pbc(sys->rx[i] - sys->rx[j], boxby2);
-            rsq = rx*rx;
-            if(rsq>rcutsq) continue; // 1D pre truncate
-            ry=pbc(sys->ry[i] - sys->ry[j], boxby2);
-            rsq += ry*ry;
-            if(rsq>rcutsq) continue; // 1D pre truncate
-            rz=pbc(sys->rz[i] - sys->rz[j], boxby2);
-            rsq += rz*rz;
-            if(rsq>rcutsq) continue; // 1D pre truncate
+                /* get distance between particle i and j */
+                rx=pbc(sys->rx[i] - sys->rx[j], boxby2);
+                rsq = rx*rx;
+                if(rsq>rcutsq) continue; // 1D pre truncate
+                ry=pbc(sys->ry[i] - sys->ry[j], boxby2);
+                rsq += ry*ry;
+                if(rsq>rcutsq) continue; // 1D pre truncate
+                rz=pbc(sys->rz[i] - sys->rz[j], boxby2);
+                rsq += rz*rz;
+                if(rsq>rcutsq) continue; // 1D pre truncate
             
-            rsq = rx*rx + ry*ry + rz*rz;
-            rsq_inv = 1.0/rsq;
-            r6 = rsq_inv*rsq_inv*rsq_inv;
-            ffac = (48*c12*r6-24*c6)*r6*rsq_inv;
-            sys->epot += 2*r6*(c12*r6-c6);
-            sys->fx[i] += rx*ffac;
-            sys->fy[i] += ry*ffac;
-            sys->fz[i] += rz*ffac;
+                rsq_inv = 1.0/rsq;
+                r6 = rsq_inv*rsq_inv*rsq_inv;
+                ffac = (48*c12*r6-24*c6)*r6*rsq_inv;
+                sys->epot += 2*r6*(c12*r6-c6);
+                sys->fx[i] += rx*ffac;
+                sys->fy[i] += ry*ffac;
+                sys->fz[i] += rz*ffac;
             
-        }
-        /* particles have no interactions with themselves */
-        // rm (i==j) decision with separated loop
-        for(j=i+1; j < sys->natoms; ++j) {
-            /* particles have no interactions with themselves */
-            
-            /* get distance between particle i and j */
-            rx=pbc(sys->rx[i] - sys->rx[j], boxby2);
-            rsq = rx*rx;
-            if(rsq>rcutsq) continue; // 1D pre truncate
-            ry=pbc(sys->ry[i] - sys->ry[j], boxby2);
-            rsq += ry*ry;
-            if(rsq>rcutsq) continue; // 1D pre truncate
-            rz=pbc(sys->rz[i] - sys->rz[j], boxby2);
-            rsq += rz*rz;
-            if(rsq>rcutsq) continue; // 1D pre truncate
-            
-            rsq = rx*rx + ry*ry + rz*rz;
-            rsq_inv = 1.0/rsq;
-            r6 = rsq_inv*rsq_inv*rsq_inv;
-            ffac = (48*c12*r6-24*c6)*r6*rsq_inv;
-            sys->epot += 2*r6*(c12*r6-c6);
-            sys->fx[i] += rx*ffac;
-            sys->fy[i] += ry*ffac;
-            sys->fz[i] += rz*ffac;
-            
+            }
         }
     }
 }
