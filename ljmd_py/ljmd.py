37d061b6b3594e43ed7ca9456f1674e84ccaac73
carolinabonivento <32060158+carolinabonivento@users.noreply.github.com>
2018-01-30 17:23:06 +0100

    progress on unit testing

--- ljmd_py/ljmd.py
+++ ljmd_py/ljmd.py
@@ -4,8 +4,8 @@ from ctypes import *
 from math import *
 import sys
 
-class parm(Structure):
-   _mdsys_ = [ ("natoms", c_int), ("nfi", c_int), ("nsteps", c_int), ("dt", c_double), ("redmass", c_double), ("epsilon", c_double), ("sigma", c_double), ("box", c_double), ("rcut", c_double), ("ekin", c_double), ("epot", c_double), ("temp", c_double), ("rx", POINTER(c_double)), ("ry", POINTER(c_double)), ("rz", POINTER(c_double)),("vx", POINTER(c_double)),("vy", POINTER(c_double)), ("vz", POINTER(c_double)), ("fx", POINTER(c_double)),("fy", POINTER(c_double)), ("fz", POINTER(c_double))]
+class MDSYS(Structure):
+   _parms_ = [ ("natoms", c_int), ("nfi", c_int), ("nsteps", c_int), ("dt", c_double), ("redmass", c_double), ("epsilon", c_double), ("sigma", c_double), ("box", c_double), ("rcut", c_double), ("ekin", c_double), ("epot", c_double), ("temp", c_double), ("rx", POINTER(c_double)), ("ry", POINTER(c_double)), ("rz", POINTER(c_double)),("vx", POINTER(c_double)),("vy", POINTER(c_double)), ("vz", POINTER(c_double)), ("fx", POINTER(c_double)),("fy", POINTER(c_double)), ("fz", POINTER(c_double))]
 
 def read():
 
@@ -26,31 +26,31 @@ if __name__ == "__main__":
     
     list=read()
     
-    sys=parm()
+    mdsys=MDSYS()
     mvsq2e=2390.05736153349
     
-    sys.natoms=int(list[0])
-    sys.redmass =float(list[1]) *mvsq2e
-    sys.epsilon=float(list[2])
-    sys.sigma=float(list[3])
-    sys.rcut=float(list[4])
-    sys.box=float(list[5])
+    mdsys.natoms=int(list[0])
+    mdsys.redmass =float(list[1]) *mvsq2e
+    mdsys.epsilon=float(list[2])
+    mdsys.sigma=float(list[3])
+    mdsys.rcut=float(list[4])
+    mdsys.box=float(list[5])
     restfile=(list[6])
     trajfile=(list[7])
     ergfile=(list[8])
-    sys.nsteps=int(list[9])
-    sys.dt=float(list[10])
+    mdsys.nsteps=int(list[9])
+    mdsys.dt=float(list[10])
     nprint=int(list[11])
     
-    print(sys.natoms)
-    print(sys.redmass)
-    print(sys.epsilon)
-    print(sys.sigma)
-    print(sys.rcut)
-    print(sys.box)
+    print(mdsys.natoms)
+    print(mdsys.redmass)
+    print(mdsys.epsilon)
+    print(mdsys.sigma)
+    print(mdsys.rcut)
+    print(mdsys.box)
     print(restfile)
     print(trajfile)
     print(ergfile)
-    print(sys.nsteps)
-    print(sys.dt)
+    print(mdsys.nsteps)
+    print(mdsys.dt)
     print(nprint)
