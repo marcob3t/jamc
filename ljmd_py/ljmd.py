98ae1bd48bf3c9242689ec33a38fa6b70b56a420
carolinabonivento <32060158+carolinabonivento@users.noreply.github.com>
2018-01-30 15:33:21 +0100

    python input working

--- ljmd_py/ljmd.py
+++ ljmd_py/ljmd.py
@@ -2,9 +2,9 @@
 
 from ctypes import *
 from math import *
-from sys import *
+import sys
 
-class parm(Srtucture):
+class parm(Structure):
    _mdsys_ = [ ("natoms", c_int), ("nfi", c_int), ("nsteps", c_int), ("dt", c_double), ("redmass", c_double), ("epsilon", c_double), ("sigma", c_double), ("box", c_double), ("rcut", c_double), ("ekin", c_double), ("epot", c_double), ("temp", c_double), ("rx", POINTER(c_double)), ("ry", POINTER(c_double)), ("rz", POINTER(c_double)),("vx", POINTER(c_double)),("vy", POINTER(c_double)), ("vz", POINTER(c_double)), ("fx", POINTER(c_double)),("fy", POINTER(c_double)), ("fz", POINTER(c_double))]
 
 def read():
@@ -18,29 +18,29 @@ def read():
     
     return (x)
 
-mvsq2e=2390.05736153349
 
 if __name__ == "__main__":
     argv = sys.argv[:]
 
-    dso = CDLL("./ljmd.so")
-    
-    sys=parm()
+    dso = CDLL("../ljmd.so")
     
     list=read()
     
-    sys.natoms=c_int(list[0])
-    sys.redmass =c_double(list[1])*mvsq2e
-    sys.epsilon=c_double(list[2])
-    sys.sigma=c_double(list[3])
-    sys.rcut=c_double(list[4])
-    sys.box=c_double(list[5])
+    sys=parm()
+    mvsq2e=2390.05736153349
+    
+    sys.natoms=int(list[0])
+    sys.redmass =float(list[1]) *mvsq2e
+    sys.epsilon=float(list[2])
+    sys.sigma=float(list[3])
+    sys.rcut=float(list[4])
+    sys.box=float(list[5])
     restfile=(list[6])
     trajfile=(list[7])
     ergfile=(list[8])
-    sys.nsteps=c_int(list[9])
-    sys.dt=c_double(list[10])
-    nprint=c_int(list[11])
+    sys.nsteps=int(list[9])
+    sys.dt=float(list[10])
+    nprint=int(list[11])
     
     print(sys.natoms)
     print(sys.redmass)
@@ -54,24 +54,3 @@ if __name__ == "__main__":
     print(sys.nsteps)
     print(sys.dt)
     print(nprint)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/
